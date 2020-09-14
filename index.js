a2bfe6b9e06fc507bcc553f4565ed42e52ab14c5
feux07 <feux07@gmail.com>
2020-09-14 22:37:00 +0300

    refactored

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 require("dotenv").config();
 
-
 const Twit = require("twit");
 
 import { sendToUser, initializeTelegramBot } from "./services/telegram";
 import { getStream, constructTweetUrl } from "./services/twitter";
 
-
 const { TELEGRAM_TARGET_CHAT_ID, TWITTER_SEARCH_TEXTS } = process.env;
 
 initializeTelegramBot();
 
-
-sendToUser(TELEGRAM_TARGET_CHAT_ID,
-  `<strong>Tstream is started...</strong>`);
-
 const stream = getStream(TWITTER_SEARCH_TEXTS);
 
+stream.on("tweet", function (tweet) {
+  let text = tweet.full_text ? tweet.full_text : tweet.text;
 
-stream.on('tweet', function (tweet) {
+  if (!text.includes("RT")) {
+    console.log(JSON.stringify(tweet));
 
-  let text = tweet.full_text ? tweet.full_text : tweet.text; 
-
-  if(!text.includes("RT")){
-    console.log(JSON.stringify(tweet)  );
-
-    sendToUser(TELEGRAM_TARGET_CHAT_ID,
-      `<strong>Content</strong> \n\n${text}\n\n\n<strong>Link</strong>\n\n${constructTweetUrl(tweet)}`);
+    sendToUser(
+      TELEGRAM_TARGET_CHAT_ID,
+      `<strong>Content</strong> \n\n${text}\n\n\n<strong>Link</strong>\n\n${constructTweetUrl(
+        tweet
+      )}`
+    );
   }
-  });
+});
--- services/telegram.js
+++ services/telegram.js
@@ -2,63 +2,59 @@ require("dotenv").config();
 
 import { Telegram, Telegraf } from "telegraf";
 
-const { TELEGRAM_BOT_TOKEN, TELEGRAM_ADMIN_USER } = process.env;
+const {
+  TELEGRAM_BOT_TOKEN,
+  TELEGRAM_ADMIN_USER,
+  TELEGRAM_TARGET_CHAT_ID,
+} = process.env;
 
 const telegram = new Telegram(TELEGRAM_BOT_TOKEN);
 
 let modeStatus = true;
 
-
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
-
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 function initializeTelegramBot() {
-  bot.start((ctx) => ctx.reply('Welcome to tstream bot'))
-  bot.command("startStream", ctx => {
-
+  bot.start((ctx) => ctx.reply("Welcome to tstream bot"));
+  bot.command("startStream", (ctx) => {
     let message = `I can only serve ${TELEGRAM_ADMIN_USER}..!`;
 
     if (ctx.update.message.from.username === TELEGRAM_ADMIN_USER) {
-
       if (modeStatus) {
-        message = "Streaming is already started..."
-      }
-      else {
+        message = "Streaming is already started...";
+      } else {
         modeStatus = true;
-        message = "Streaming is started..."
+        message = "Streaming is started...";
       }
     }
 
     return ctx.reply(message);
   });
-  bot.command("stopStream", ctx => {
-
+  bot.command("stopStream", (ctx) => {
     let message = `I can only serve ${TELEGRAM_ADMIN_USER}..!`;
 
     if (ctx.update.message.from.username === TELEGRAM_ADMIN_USER) {
-
       if (!modeStatus) {
-        message = "Streaming is already stopped..."
-      }
-      else {
+        message = "Streaming is already stopped...";
+      } else {
         modeStatus = false;
-        message = "Streaming is stopped..."
+        message = "Streaming is stopped...";
       }
     }
 
     return ctx.reply(message);
   });
-  bot.launch()
+  bot.launch();
+  sendToUser(TELEGRAM_TARGET_CHAT_ID, `<strong>Tstream is started...</strong>`);
 }
 
 function sendToUser(to, message) {
-
   if (modeStatus) {
-    telegram.sendMessage(to, message, { disable_web_page_preview: true, parse_mode: "HTML" });
+    telegram.sendMessage(to, message, {
+      disable_web_page_preview: true,
+      parse_mode: "HTML",
+    });
   }
-
-
-
 }
 
 export { sendToUser, initializeTelegramBot };
